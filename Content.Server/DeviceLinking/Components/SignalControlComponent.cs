5c380b4cff0da3ffa95f277803178f048c87cb94
TheMarkie <5463623+TheMarkie@users.noreply.github.com>
2024-07-05 01:51:37 +0700

    added generic signal control component

---
+++ Content.Server/DeviceLinking/Components/SignalControlComponent.cs
@@ -0,0 +1,25 @@
+using Content.Server.DeviceLinking.Systems;
+using Content.Shared.DeviceLinking;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.DeviceLinking.Components
+{
+    /// <summary>
+    /// A component with an On/Off state controlled by signals from DeviceLinking
+    /// </summary>
+    [RegisterComponent, Access(typeof(SignalControlSystem))]
+    public sealed partial class SignalControlComponent : Component
+    {
+        [DataField]
+        public ProtoId<SinkPortPrototype> TogglePort = "Toggle";
+
+        [DataField]
+        public ProtoId<SinkPortPrototype> OnPort = "On";
+
+        [DataField]
+        public ProtoId<SinkPortPrototype> OffPort = "Off";
+
+        [ViewVariables(VVAccess.ReadWrite)]
+        public bool IsOn = false;
+    }
+}
