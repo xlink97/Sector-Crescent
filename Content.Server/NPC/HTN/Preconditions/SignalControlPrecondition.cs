c67d5c286c53515d70c68389a9892ef6a4464604
cherubdev <155099677+cherubdev@users.noreply.github.com>
2024-07-04 23:52:17 +0300

    turretcode (#23)
    * added generic signal control component
    
    * added signal control precondition to ai behaviours
    
    * added a default state to SignalControl
    
    * added powered precondition to ai behaviours
    
    * added default state if signal control precondition is missing the component
    
    * tweaked default value of new preconditions
    
    ---------
    
    Co-authored-by: ilikeships <105243738+ilikeships@users.noreply.github.com>
    Co-authored-by: TheMarkie <5463623+TheMarkie@users.noreply.github.com>

---
+++ Content.Server/NPC/HTN/Preconditions/SignalControlPrecondition.cs
@@ -0,0 +1,30 @@
+using Content.Server.DeviceLinking.Components;
+
+namespace Content.Server.NPC.HTN.Preconditions
+{
+    /// <summary>
+    /// Returns true if the entity has a SignalControl component in On state
+    /// </summary>
+    public sealed partial class SignalControlPrecondition : HTNPrecondition
+    {
+        [Dependency] private readonly IEntityManager _entManager = default!;
+
+        [DataField("isOnIfNoSignalControl")]
+        public bool IsOnIfNoSignalControl = false;
+
+        public override bool IsMet(NPCBlackboard blackboard)
+        {
+            if (blackboard.TryGetValue<EntityUid>(NPCBlackboard.Owner, out var owner, _entManager))
+            {
+                if (_entManager.TryGetComponent<SignalControlComponent>(owner, out var control))
+                {
+                    return control.IsOn;
+                }
+
+                return IsOnIfNoSignalControl;
+            }
+
+            return false;
+        }
+    }
+}
